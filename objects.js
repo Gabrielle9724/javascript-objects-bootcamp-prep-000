8de92722a3d99b70e92cc0cb8fdf2655685c9097
Gabrielle Piacentile <gabriellepiacentile98@gmail.com>
2020-12-12 20:25:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new object({vulfpeck: backpocket})
+var playlist = new object ({vulfpeck: backpocket})
 
 function updatePlaylist (playlist, artistName, songTitle){
   playlist[artistName] = songTitle
